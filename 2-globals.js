369b036c4b88764eb05467bfb96994bf501094b1
Laurentiu Pomana <laurentiupomana@Laurentius-MacBook-Pro.local>
2022-05-08 18:12:14 +0300

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,17 @@
+/* 
+
+GLOBALS - NO WINDOW
+__dirname = path to current diretory
+__filename = file name
+require = function to use module (common JS)
+module = function to show info about the module (file)
+process = info about environment where the program is executed
+
+*/
+
+
+console.log(__dirname);
+
+setInterval(() => {
+    console.log(`hello there`);
+},1000);
---
+++ 5-utils.js
@@ -0,0 +1,9 @@
+
+
+
+const sayHi = (name) =>{
+    console.log(`hello there ${name}`);
+};
+
+module.exports = sayHi;
+
